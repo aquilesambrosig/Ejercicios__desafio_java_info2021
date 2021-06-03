6aef205ae64c8518de8691a725f3de2fbbe06e58
aquilesambrosig <aquiles-ambrosig@hotmail.com>
2021-06-03 14:33:17 -0300

    v2

--- ListaEmpleados.java
+++ ListaEmpleados.java
@@ -36,5 +36,22 @@ public class ListaEmpleados {
         
     }
 
+    class Empleado {
+        int dni;
+        String nombre;
+        String apellido;
+        int horasTrabajadas;
+        int valoPorHora;
+    
+        public Empleado( String nombre, String apellido, int dni, int horasTrabajadas, int valorPorHora) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+            this.horasTrabajadas = horasTrabajadas;
+            this.valoPorHora = valorPorHora;
+            
+        }
+    }
+
 
 
